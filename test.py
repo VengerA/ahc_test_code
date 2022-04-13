64cb7a806bc6716c42a44c4c1f3a2447f8993f98
ufukyagmur <uyagmur123@gmail.com>
2022-04-13 22:01:05 +0300

    asd

--- test.py
+++ test.py
@@ -1,36 +1,13 @@
 from cgi import print_exception
 import time, random, math
 from enum import Enum
-from adhoccomputing import GenericModel, GenericEvent, Generics, Definitions, Topology
+from tkinter import Frame
+from adhoccomputing import GenericModel, Event, Generics, Definitions, Topology, FramerObjects, FrameHandlerBase, ofdm_callback, MacCsmaPPersistentConfigurationParameters, MacCsmaPPersistent
 from LiquidDsputils import *
 from Uhdutils import *
 from ctypes import *
-
 import queue
 import pickle
-# from ahc import ComponentModel, Event, ConnectorTypes, Topology, EventTypes, GenericMessage, GenericMessageHeader, FramerObjects
-# from ahc import ComponentRegistry
-# from ahc.PhysicalLayers.UsrpB210OfdmFlexFramePhy import  UsrpB210OfdmFlexFramePhy
-# from ahc.MAC.CSMA import MacCsmaPPersistent,MacCsmaPPersistentConfigurationParameter
-
-class FramerObjects():
-    framerobjects = {}
-    ahcuhdubjects = {}
-    def add_framer(self, id, obj):
-        self.framerobjects[id] = obj
-
-    def get_framer_by_id(self, id):
-        return self.framerobjects[id]
-
-    def add_ahcuhd(self, id, obj):
-        self.ahcuhdubjects[id] = obj
-
-    def get_ahcuhd_by_id(self, id):
-        return self.ahcuhdubjects[id]
-
-
-
-framers = FramerObjects()
 
 
 # define your own message types
@@ -48,19 +25,19 @@ class UsrpApplicationLayerEventTypes(Enum):
 
 
 class UsrpApplicationLayer(GenericModel):
-    # def on_init(self, eventobj: GenericEvent):
+    # def on_init(self, eventobj: Event):
 
     def __init__(self, componentname, componentid):
         super().__init__(componentname, componentid)
         self.counter = 0
         self.eventhandlers[UsrpApplicationLayerEventTypes.STARTBROADCAST] = self.on_startbroadcast
 
-    def on_message_from_top(self, eventobj: GenericEvent):
+    def on_message_from_top(self, eventobj: Event):
     # print(f"I am {self.componentname}.{self.componentinstancenumber},sending down eventcontent={eventobj.eventcontent}\n")
-        self.send_down(GenericEvent(self, Definitions.EventTypes.MFRT, eventobj.eventcontent))
+        self.send_down(Event(self, Definitions.EventTypes.MFRT, eventobj.eventcontent))
 
-    def on_message_from_bottom(self, eventobj: GenericEvent):
-        evt = GenericEvent(self, Definitions.EventTypes.MFRT, eventobj.eventcontent)
+    def on_message_from_bottom(self, eventobj: Event):
+        evt = Event(self, Definitions.EventTypes.MFRT, eventobj.eventcontent)
         print(f"I am Node.{self.componentinstancenumber}, received from Node.{eventobj.eventcontent.header.messagefrom} a message: {eventobj.eventcontent.payload}")
         if self.componentinstancenumber == 1:
             evt.eventcontent.header.messageto = 0
@@ -72,7 +49,7 @@ class UsrpApplicationLayer(GenericModel):
         #print(f"I am {self.componentname}.{self.componentinstancenumber}, sending down eventcontent={eventobj.eventcontent.payload}\n")
         self.send_down(evt)  # PINGPONG
 
-    def on_startbroadcast(self, eventobj: GenericEvent):
+    def on_startbroadcast(self, eventobj: Event):
         if self.componentinstancenumber == 1:
             hdr = ApplicationLayerMessageHeader(ApplicationLayerMessageTypes.BROADCAST, 1, 0)
         else:
@@ -81,44 +58,10 @@ class UsrpApplicationLayer(GenericModel):
 
         payload = "BMSG-" + str(self.counter)
         broadcastmessage = Generics.GenericMessage(hdr, payload)
-        evt = GenericEvent(self, Definitions.EventTypes.MFRT, broadcastmessage)
+        evt = Event(self, Definitions.EventTypes.MFRT, broadcastmessage)
         # time.sleep(3)
         self.send_down(evt)
 
-    def send_self(self, event: GenericEvent):
-        self.trigger_event(event)
-
-mutex = Lock()
-def ofdm_callback(header:POINTER(c_ubyte), header_valid:c_uint32, payload:POINTER(c_ubyte), payload_len:c_uint32, payload_valid:c_int32, stats:struct_c__SA_framesyncstats_s, userdata:POINTER(None)):
-    mutex.acquire(1)
-    try:
-        framer = framers.get_framer_by_id(userdata)
-        #print("ofdm_callback", framer.componentinstancenumber)
-        #ofdmflexframegen_print(framer.fg)
-        # userdata.debug_print()
-        #print("Type", type(payload), "Payload Valid?: ", payload_valid, "Length=", payload_len, "payload=", bytes(payload))
-
-        if payload_valid != 0:
-            #ofdmflexframesync_print(framer.fs)
-            pload = string_at(payload, payload_len)
-            #print("pload=", pload)
-            phymsg = pickle.loads(pload)
-            msg = Generics.GenericMessage(phymsg.header, phymsg.payload)
-            framer.send_self(GenericEvent(framer, UsrpB210PhyEventTypes.RECV, msg))
-            #print("Header=", msg.header.messagetype, " Payload=", msg.payload, " RSSI=", stats.rssi)
-        #else:
-            #pass
-        #print("INVALID Type Node", framer.componentinstancenumber, "Payload Valid:[", payload_valid, "]Length=", payload_len, "payload=", bytes(payload))
-
-    except Exception as e:
-        print("Exception_ofdm_callback:", e)
-        print("INVALID Type Node", framer.componentinstancenumber, "Payload Valid:[", payload_valid, "]Length=", payload_len, "payload=", bytes(payload))
-    mutex.release()
-    return 0
-
-
-class UsrpB210PhyEventTypes(Enum):
-  RECV = "recv"
 
 
 # define your own message header structure
@@ -133,71 +76,13 @@ class UsrpB210PhyMessagePayload(Generics.GenericMessagePayload):
     self.phyheader = header
     self.phypayload = payload
 
-class FrameHandlerBase(GenericModel):
-
-    def __init__(self,componentname, componentinstancenumber):
-        super().__init__(componentname, componentinstancenumber)
-
-        self.chan = 0
-        self.bandwidth = 250000
-        self.freq = 2462000000.0
-        self.lo_offset = 0
-        self.rate = self.bandwidth
-        self.hw_tx_gain = 70.0  # hardware tx antenna gain
-        self.hw_rx_gain = 20.0  # hardware rx antenna gain
-        self.sw_tx_gain = -12.0  # software gain
-        self.duration = 1
-        self.ahcuhd = AhcUhdUtils(self.componentinstancenumber)
-        framers.add_framer(id(self), self)
-        # framers.add_ahcuhd(componentinstancenumber, self.ahcuhd )
-        self.ahcuhd.configureUsrp("winslab_b210_" + str(self.componentinstancenumber))
-        print("Configuring", "winslab_b210_" + str(self.componentinstancenumber))
-        self.configure()
-        self.eventhandlers[UsrpB210PhyEventTypes.RECV] = self.on_recv
-
-    def on_recv(self, eventobj: GenericEvent):
-        #print("Node", self.componentinstancenumber, " Received message type:", eventobj.eventcontent.header.messagetype, "  from ", eventobj.eventcontent.payload.phyheader.messagefrom)
-
-        if eventobj.eventcontent.payload.phyheader.messagefrom != self.componentinstancenumber:
-          msg = Generics.GenericMessage(eventobj.eventcontent.payload.phyheader, eventobj.eventcontent.payload.phypayload)
-          self.send_up(GenericEvent(self, Definitions.EventTypes.MFRB, msg))
-
-
-
-
-    def on_message_from_top(self, eventobj: GenericEvent):
-    # channel receives the input message and will process the message by the process event in the next pipeline stage
-    # Preserve the event id through the pipeline
-
-        str_header = "12345678"  #This is the PMD flexframe header. Ourt physical layer header will be concat with the payload below...
-        hlen = len(str_header)
-        byte_arr_header = bytearray(str_header, 'utf-8')
-        header = (c_ubyte * hlen)(*(byte_arr_header))
-
-        hdr = UsrpB210PhyMessageHeader(UsrpB210PhyMessageTypes.PHYFRAMEDATA, self.componentinstancenumber, Definitions.MessageDestinationIdentifiers.LINKLAYERBROADCAST)
-        pld = UsrpB210PhyMessagePayload(eventobj.eventcontent.header, eventobj.eventcontent.payload )
-        msg = Generics.GenericMessage(hdr, pld)
-        byte_arr_msg = bytearray(pickle.dumps(msg))
-        plen = len(byte_arr_msg)
-        payload = (c_ubyte * plen)(*(byte_arr_msg))
-        payload_len = plen
-        #print("bytearry:", byte_arr_msg, "Payload:",payload, " payload_len:", payload_len)
-        self.transmit(header, payload, payload_len, LIQUID_MODEM_QPSK, LIQUID_FEC_NONE, LIQUID_FEC_HAMMING74 )  # TODO: Check params
-        #print("sentpload=", string_at(payload, payload_len))
-        #pload = string_at(payload, payload_len)
-        #print("pload=", pload)
-        #phymsg = pickle.loads(pload)
-        #msg2 = GenericMessage(phymsg.header, phymsg.payload)
-
-class UsrpB210PhyMessageTypes(Enum):
-  PHYFRAMEDATA = "PHYFRAMEDATA"
 class UsrpB210OfdmFlexFramePhy(FrameHandlerBase):
 
-    def on_init(self, eventobj: GenericEvent):
+    def on_init(self, eventobj: Event):
         #print("initialize LiquidDspOfdmFlexFrameHandler")
         pass
 
-    def send_self(self, event: GenericEvent):
+    def send_self(self, event: Event):
         self.trigger_event(event)
 
     def rx_callback(self, num_rx_samps, recv_buffer):
@@ -267,100 +152,10 @@ class UsrpB210OfdmFlexFramePhy(FrameHandlerBase):
     def __init__(self, componentname, componentinstancenumber):
         super().__init__(componentname, componentinstancenumber)
 
-class ComponentConfigurationParameters():
-    pass
-
-class MacCsmaPPersistentConfigurationParameters (ComponentConfigurationParameters):
-    def __init__(self, p):
-        self.p = p
-
-class GenericMacEventTypes(Enum):
-    HANDLEMACFRAME = "handlemacframe"
-
-
-class GenericMac(GenericModel):
-
-    def __init__(self, componentname, componentinstancenumber, uhd):
-        super().__init__(componentname, componentinstancenumber)
-        self.framequeue = queue.Queue()
-        self.ahcuhd = uhd
-        print("I am Generic MAC my uhd instance id is ", self.ahcuhd.componentinstancenumber)
-        self.eventhandlers[GenericMacEventTypes.HANDLEMACFRAME] = self.on_handlemacframe
-
-
-    def on_init(self, eventobj: GenericEvent):
-        self.send_self(GenericEvent(self, GenericMacEventTypes.HANDLEMACFRAME, None))  # Continuously trigger handle_frame
-        print("Initialized", self.componentname, ":", self.componentinstancenumber)
-
-    def on_handlemacframe(self, eventobj: GenericEvent):
-        # print("will handle frame from on_handlemacframe")
-        self.handle_frame()
-        # self.send_self(Event(self, GenericMacEventTypes.HANDLEMACFRAME, None)) #Continuously trigger handle_frame
-        # HANDLEMACFRAME event will be generated by the inheriting component to facilitate delay
-
-    def on_message_from_bottom(self, eventobj: GenericEvent):
-        # print(f"I am {self.componentname}, eventcontent={eventobj.eventcontent}\n")
-        evt = GenericEvent(self, Definitions.EventTypes.MFRB, eventobj.eventcontent)
-        self.send_up(evt)
-
-    def on_message_from_top(self, eventobj: GenericEvent):
-        # print(f"I am {self.componentname}, eventcontent={eventobj.eventcontent}\n")
-        # put message in queue and try accessing the channel
-        # self.on_handlemacframe(eventobj)
-        self.framequeue.put_nowait(eventobj)
-        self.handle_frame()
-        #print("Mac put the frame in queueu", eventobj.eventcontent.payload)
-
-    def handle_frame(self):
-        pass
-
-class MacCsmaPPersistent(GenericMac):
-
-    #Constructor
-    def __init__(self, componentname, componentinstancenumber, configurationparameters:MacCsmaPPersistentConfigurationParameters, uhd):
-        super().__init__(componentname, componentinstancenumber, uhd)
-        self.p = configurationparameters.p
-
-    #on_init will be called from topo.start to initialize components
-    def on_init(self, eventobj: GenericEvent):
-        self.retrialcnt = 0
-        super().on_init(eventobj)  # required because of inheritence
-        #print("Initialized", self.componentname, ":", self.componentinstancenumber)
-
-    def send_self(self, event: GenericEvent):
-        self.trigger_event(event)
-
-    def handle_frame(self):
-        #TODO: not a good solution put message in queue, schedule a future event to retry yhe first item in queueu
-        # print("handle_frame")
-        if self.framequeue.qsize() > 0:
-            randval = random.random()
-            if randval < self.p: # TODO: Check if correct
-                clearmi, powerdb  = self.ahcuhd.ischannelclear(threshold=-35)
-                #print("Component:", self.componentinstancenumber, "clear mi=", clearmi, " Power=", powerdb)
-                if  clearmi == True:
-                    try:
-                        eventobj = self.framequeue.get()
-                        evt = GenericEvent(self, Definitions.EventTypes.MFRT, eventobj.eventcontent)
-                        self.send_down(evt)
-                        self.retrialcnt = 0
-                    except Exception as e:
-                        raise(e)
-                        print("MacCsmaPPersistent handle_frame exception, ")
-                else:
-                    self.retrialcnt = self.retrialcnt + 1
-                    time.sleep(random.randrange(0,math.pow(2,self.retrialcnt))*0.001)
-                    #print("Busy")
-        else:
-            #print("Queue size", self.framequeue.qsize())
-            pass
-        time.sleep(0.00001) # TODO: Think about this otherwise we will only do cca
-        self.send_self(GenericEvent(self, GenericMacEventTypes.HANDLEMACFRAME, None)) #Continuously trigger handle_frame
-
 
 class UsrpNode(GenericModel):
     counter = 0
-    def on_init(self, eventobj: GenericEvent):
+    def on_init(self, eventobj: Event):
         pass
 
     def __init__(self, componentname, componentid):
@@ -370,7 +165,7 @@ class UsrpNode(GenericModel):
 
         self.appl = UsrpApplicationLayer("UsrpApplicationLayer", componentid)
         self.phy = UsrpB210OfdmFlexFramePhy("UsrpB210OfdmFlexFramePhy", componentid)
-        self.mac = MacCsmaPPersistent("MacCsmaPPersistent", componentid,  configurationparameters=macconfig,uhd=self.phy.ahcuhd)
+        self.mac = MacCsmaPPersistent("MacCsmaPPersistent", componentid,  configurationparameters=macconfig, uhd=self.phy.ahcuhd)
 
         # CONNECTIONS AMONG SUBCOMPONENTS
         self.appl.connect_me_to_component(Definitions.ConnectorTypes.UP, self) #Not required if nodemodel will do nothing
@@ -401,7 +196,7 @@ def main():
     topo.start()
     i = 0
     while(i < 10):
-        topo.nodes[1].appl.send_self(GenericEvent(topo.nodes[0], UsrpApplicationLayerEventTypes.STARTBROADCAST, None))
+        topo.nodes[1].appl.send_self(Event(topo.nodes[0], UsrpApplicationLayerEventTypes.STARTBROADCAST, None))
         time.sleep(1)
         i = i + 1
 
